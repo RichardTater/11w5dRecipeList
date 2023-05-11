bdffc7575be759d2e57dcdf293e1bf88d3a9d5aa
Lane Vance <lanevance92@gmail.com>
2023-05-11 15:38:03 -0600

    part 3.2 ish. and doing styles for Detail screen

--- src/elements/RecipeCard.jsx
+++ src/elements/RecipeCard.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import styles from "./RecipeCard.module.css";
+import { useNavigate } from "react-router-dom";
 
-function RecipeCard() {
+function RecipeCard({recipe}) {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(`/recipe/${recipe.recipe_id}`)
+  }
   return (
     <div className={styles.recipe_card}>
       <div>
         <div className={styles.recipe_img_container}>
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz2WjumF-nm50Wd9cbmztbdUzOPsRFevFTIlk-qZhu6w&usqp=CAU&ec=48665698"
-            alt="Grilled Watermellon"
-          />
+        <img src={recipe.image_url} />
         </div>
-        <h3 className={styles.cardH3}>Grilled Watermellon</h3>
-      <button className={styles.recipeBtn}>See More</button>
+        <h3 className={styles.cardH3}>{recipe.recipe_name}</h3>
+      <button className={styles.recipeBtn} onClick={handleClick}>See More</button>
       </div>
     </div>
   );
